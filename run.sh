471d164ec87ef38fb0b9890de611d50014204522
yalidu <yali.du86@gmail.com>
2021-02-02 22:22:40 +0800

    minor on changing shell

--- run.sh
+++
@@ -1,3 +0,0 @@
-python main_traffic.py --env_name cacc_slowdown  --nagents 8 --ic3net --seed 1
-python main_traffic.py --env_name cacc_slowdown  --nagents 8 --ic3net --seed 5
-python main_traffic.py --env_name cacc_slowdown  --nagents 8 --ic3net --seed 7
---
+++ train-catchup.sh
@@ -0,0 +1,5 @@
+python main_traffic.py --env_name cacc_catchup  --nagents 8 --ic3net --seed 13
+python main_traffic.py --env_name cacc_catchup  --nagents 8 --ic3net --seed 17
+python main_traffic.py --env_name cacc_catchup  --nagents 8 --ic3net --seed 19
+#python main_traffic.py --env_name cacc_catchup  --nagents 8 --ic3net --seed 23
+#python main_traffic.py --env_name cacc_catchup  --nagents 8 --ic3net --seed 29
